6a5cb5b5c2c3e3ab0a34cbd8f1de717b0aca6215
Jade-Banares <proj.jade.banares@gmail.com>
2022-10-01 03:20:21 +0800

    files added

---
+++ js/music-list.js
@@ -0,0 +1,86 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Found this girl",
+    artist: "The vamps",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Wake Up",
+    artist: "The Vamps",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "She Looks So Perfect",
+    artist: "5SOS",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Beatbox",
+    artist: "NCT Dream",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Amnesia",
+    artist: "5SOS",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Breakeven",
+    artist: "The Script",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Paradise",
+    artist: "Coldplay",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Oh Cecilia",
+    artist: "The Vamps ft. Shawn Mendes",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "A Sky Full Of Stars",
+    artist: "Coldplay",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Wake Me Up",
+    artist: "Avicii",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "Can't Stop The Feeling",
+    artist: "Justin Timberlake",
+    img: "music-11",
+    src: "music-11"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
